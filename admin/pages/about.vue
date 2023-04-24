f25dee47d0721c91215d938d38c1f5c4321bff8b
matheusmaldonadosilva <matheussilvacontat@gmail.com>
2023-04-23 23:50:46 -0300

    update project amazon page

--- admin/pages/about.vue
+++ admin/pages/about.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>About Me</h1>
+  <div>
+    <div >
+      <h1></h1>
+    </div>
+  </div>
 </template>
+
+<script>
+
+
+
+export default {
+  //asyncData is fetching data before nuxt page app fisished loading on the broser.
+  //it is good for SEO because the data will be loaded first
+  async asyncData({$axios}) {
+    try {
+      console.log('oi');
+      let response = await $axios.$get('http://localhost:3000/api/products')
+      console.log('ola');
+      return {
+        products: response.products
+      }
+    } catch(err) {
+
+    }
+  }
+
+};
+</script>
--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,72 +1,76 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        admin
-      </h1>
-      <h2 class="subtitle">
-        My splendid Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main>
+    <div class="a-spacing-large"></div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div class="col-sm-8 col-8">
+          <h1 class="a-size-large a-spacing-none a-text-normal">Todos produtos</h1>
+          <div class="a-spacing-large"></div>
+          <!--button-->
+          <a href="#" class="a-button-buy-again">Adicionar um novo produto</a>
+          <a href="#" class="a-button-history margin-right-10">Adicionar uma nova categoria</a>
+          <a href="#" class="a-button-history margin-right-10">Adicionar um novo proprietário</a>
+          <!--listing page-->
+        </div>
       </div>
     </div>
-  </div>
+    <div class="a-spacing-large"></div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div v-for="(product, index) in products" :key="product._id" class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+          <div class="history-box">
+            <!--product image-->
+            <!-- <a href="#" class="a-link-normal">
+              <img src="#" alt="" class="img-fluid">
+            </a> -->
+            <!--product title-->
+            <div class="a-spacing-top-base asin-title">
+              <span class="a-text-normal">
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
+              </span>
+            </div>
+            <!--product rating-->
+            <div class="a-row">
+              <a href="">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <div class="a-letter-space"></div>
+                <div class="a-color-tertiary a-size-small asin-reviews">(1373)</div>
+              </a>
+            </div>
+            <!--product price-->
+            <div class="a-row">
+              <span class="a-size-base a-color-price">
+                <span class="p13n-sc-price">R${{ product.price }}</span>
+              </span>
+            </div>
+
+            <!--product buttons-->
+            <div class="a-row">
+              <a href="" class="a-button-history margin-right-10">Update</a>
+              <a href="" class="a-button-history margin-right-10">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  async asyncData({ $axios }) {
+    try {
+      let response = await $axios.$get("http://localhost:2000/api/products");
+      return {
+        products: response.products,
+      };
+    } catch (err) {}
   }
-}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
